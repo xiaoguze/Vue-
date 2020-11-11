ffe27cd1edfa0bc10af0fa6379a4d23c3dcabc64
xiaoze <2393015415@qq.com>
2020-11-11 16:30:32 +0800

    路由的配置

---
+++ 15-路由/02_meet_vue_router/src/router/index.js
@@ -0,0 +1,18 @@
+import vueRouter from 'vue-router' // 引入路由
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//  使用 vue.use 安装插件
+Vue.use(vueRouter) // 安装插件
+
+
+const routes = [
+  // 配置 路劲和组件的映射关系
+  
+
+]
+
+
+// 创建 vue-router 对象
+const router = new VueRouter({
+  routes
+})
